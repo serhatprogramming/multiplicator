55c0ac97d4b5d4bdda04fc71bab7c48dbedc9660
serhatprogramming <serhatprogramming@gmail.com>
2023-05-09 13:10:37 -0400

    error handling, type unknown instanceof check

--- multiplier.ts
+++ multiplier.ts
@@ -6,17 +6,26 @@ multiplicator(2, 4, "Multiplied numbers 2 and 4, the result is:");
 
 type Operation = "multiply" | "add" | "divide";
 
-const calculator = (
-  a: number,
-  b: number,
-  op: Operation
-): number | string | unknown => {
-  if (op === "multiply") {
-    return a * b;
-  } else if (op === "add") {
-    return a + b;
-  } else if (op === "divide") {
-    if (b === 0) return "can't divide by 0!";
-    return a / b;
+const calculator = (a: number, b: number, op: Operation): number => {
+  switch (op) {
+    case "multiply":
+      return a * b;
+    case "divide":
+      if (b === 0) throw new Error("Can't divide by 0!");
+      return a / b;
+    case "add":
+      return a + b;
+    default:
+      throw new Error("Operation is not multiply, add or divide!");
   }
 };
+
+try {
+  console.log(calculator(1, 5, "divide"));
+} catch (error: unknown) {
+  let errorMessage = "Something went wrong: ";
+  if (error instanceof Error) {
+    errorMessage += error.message;
+  }
+  console.log(errorMessage);
+}
