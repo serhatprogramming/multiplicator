5790f3187946831652f2ed6dda07c436c6c769f6
serhatprogramming <serhatprogramming@gmail.com>
2023-05-09 13:08:28 -0400

    typescript multiplier

---
+++ multiplier.ts
@@ -0,0 +1,22 @@
+const multiplicator = (a: number, b: number, printText: string) => {
+  console.log(printText, a * b);
+};
+
+multiplicator(2, 4, "Multiplied numbers 2 and 4, the result is:");
+
+type Operation = "multiply" | "add" | "divide";
+
+const calculator = (
+  a: number,
+  b: number,
+  op: Operation
+): number | string | unknown => {
+  if (op === "multiply") {
+    return a * b;
+  } else if (op === "add") {
+    return a + b;
+  } else if (op === "divide") {
+    if (b === 0) return "can't divide by 0!";
+    return a / b;
+  }
+};
