5f113a7621580541d112c18fa50a606368f9dc71
serhatprogramming <serhatprogramming@gmail.com>
2023-05-09 13:37:27 -0400

    multiply arguments from terminal

--- multiplier.ts
+++ multiplier.ts
@@ -1,5 +1,6 @@
 const multiplicator = (a: number, b: number, printText: string) => {
   console.log(printText, a * b);
 };
-
-multiplicator(2, 4, "Multiplied numbers 2 and 4, the result is:");
+const a: number = Number(process.argv[2]);
+const b: number = Number(process.argv[3]);
+multiplicator(a, b, `Multiplied ${a} and ${b}, the result is:`);
